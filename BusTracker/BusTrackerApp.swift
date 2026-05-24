fe0be621130d400be9da60786e7dba3201044444
Mert Koksal <mert.koksal@machinarium.co>
2026-05-24 16:41:46 +0300

    Initial Commit

---
+++ BusTracker/BusTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTrackerApp.swift
+//  BusTracker
+//
+//  Created by Mert Köksal on 24.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct BusTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
